2a0092a763f94dfa87d2087dc7447f5fb576aeef
sunny920405 <sunny040518@gmail.com>
2023-07-11 17:36:16 +0800

    message

--- 5. ROS/src/wheeltec_multi/scripts/leadercar.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-### BEGIN INIT
-gnome-terminal -- bash -c "source /opt/ros/melodic/setup.bash;source /home/wheeltec/wheeltec_robot/devel/setup.bash;roslaunch wheeltec_multi navigation.launch"
-sleep 10
-
-wait
-exit 0
